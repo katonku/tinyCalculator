6060da78df1410d35015cb0ae48056a40ae424e5
admin <admin@MacBook-Pro-admin.local>
2025-05-24 12:09:45 +0300

    funcs were added

--- tinyCalculator/main.swift
+++ tinyCalculator/main.swift
@@ -1,55 +1,60 @@
 import Darwin
 
 print("Введите первое число: ")
-var a: Double = 0
-if let string = readLine(),
-    !string.isEmpty,
-    let doubleString = Double(string) {
-    a = doubleString
-} else {
-    print("Ошибка: введено не число")
-    exit(1)
-}
+let a = getNumberFromLine()
 
 print("Введите второе число: ")
-var b: Double = 0
-if let string = readLine(),
-   !string.isEmpty,
-   let doubleString = Double(string){
-    b = doubleString
-} else {
-    print("Ошибка: введено не число")
-    exit(1)
-}
+let b = getNumberFromLine()
 
 print("Введите операцию (+, -, *, /): ")
-var option: String = ""
-if let string = readLine(),
-   !string.isEmpty {
-    option = string
-} else {
-    print("Ошибка: введена не операция")
-    exit(1)
-}
+let option = getLineFromUser()
 
-var result: Double = 0
+let expressionView = "\(a) \(option) \(b)"
 
-switch option {
-case "+":
-    result = a + b
-case "-":
-    result = a - b
-case "*":
-    result = a * b
-case "/":
-    if b == 0 {
-        print("На ноль делить запрещено!")
+let result = makeCalculation(firstOperand: a, secondOperand: b, operation: option)
+showResult(expressionView: expressionView, result)
+
+func getLineFromUser() -> String{
+    if let string = readLine(),
+        !string.isEmpty{
+        return string
+    } else {
+        print("Ошибка: введено неверное значение")
         exit(1)
     }
-    result = a / b
-default:
-    print("Ошибка: введена не операция")
-    exit(1)
 }
 
-print("\(a) \(option) \(b) = \(result)")
+func getNumberFromLine() -> Double {
+    if let doubleString = Double(getLineFromUser()) {
+        return doubleString
+    } else {
+        print("Ошибка: введено не число")
+        exit(1)
+    }
+}
+
+func makeCalculation(firstOperand a:Double,
+                     secondOperand b:Double,
+                     operation option:String) -> Double{
+    switch option {
+    case "+":
+        return a + b
+    case "-":
+        return a - b
+    case "*":
+        return a * b
+    case "/":
+        if b == 0 {
+            print("На ноль делить запрещено!")
+            exit(1)
+        }
+        return a / b
+    default:
+        print("Ошибка: введена не операция")
+        exit(1)
+    }
+}
+
+func showResult(expressionView expression:String, _ result: Double){
+    print(expression + " = \(result)")
+}
