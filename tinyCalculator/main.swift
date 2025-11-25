9a6a7394d41dfc4d832188c4655efdc1a0d241a9
Anton <kalinichenko.anton.v@gmail.com>
2025-11-26 00:16:35 +0300

    funcs were added

--- tinyCalculator/main.swift
+++ tinyCalculator/main.swift
@@ -1,36 +1,48 @@
 print("Добро пожаловать в Tiny Calculator!")
 
-print("Введите операцию")
-let operation = readLine() ?? ""
-
-print("Введите первое число")
-let firstNumber = readLine() ?? ""
-
-print("Введите второе число")
-let secondNumber = readLine() ?? ""
+let operation = getUserData(description: "Введите операцию")
+let firstNumber = getUserData(description: "Введите первое число")
+let secondNumber = getUserData(description: "Введите второе число")
 
 print("Идёт вычисление примера " + firstNumber + " " + operation + " " + secondNumber)
 
 if let firstNumber = Int(firstNumber) {
     if let secondNumber = Int(secondNumber){
-        switch operation {
-        case "+": print("Результат " + String(firstNumber + secondNumber))
-        case "-": print("Результат " + String(firstNumber - secondNumber))
-        case "*": print("Результат " + String(firstNumber * secondNumber))
-        case "/":
-            if secondNumber != 0 {
-                print("Результат " + String(firstNumber / secondNumber))
-            } else {
-                print("Деление на ноль запрещено.")
-            }
-            
-        default: print("Операция введена неверно.")
-        }
+        calculate(operation: operation,
+                  firstNumber: firstNumber,
+                  secondNumber: secondNumber)
     } else {
         print("Второе число введено неправильно.")
     }
 } else {
     print("Первое число введено неправильно.")
+}
+
+func getUserData (description: String) -> String {
+    print(description)
+    return readLine() ?? ""
+}
+
+func printResult(_ result: Int) {
+    print("Результат \(result)")
+}
+
+func calculate(operation: String,
+               firstNumber: Int,
+               secondNumber: Int) {
+    switch operation {
+    case "+": printResult(firstNumber + secondNumber)
+    case "-": printResult(firstNumber - secondNumber)
+    case "*": printResult(firstNumber * secondNumber)
+    case "/":
+        if secondNumber != 0 {
+            printResult(firstNumber / secondNumber)
+        } else {
+            print("Деление на ноль запрещено.")
+        }
+        
+    default: print("Операция введена неверно.")
+    }
 }
 
 
